dcc12f25b29fe77a12e3788d5dd28ddd75d4c9aa
tybeewx <tybeewx@users.noreply.github.com>
2020-05-06 15:49:56 -0400

    first commit

---
+++ build/precache-manifest.a5a632e295c4658a6e234ee5b8857771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb6228d8661d8944f8b876e31b868e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64b737d26d65bca641e5",
+    "url": "/static/css/main.67ae2f2c.chunk.css"
+  },
+  {
+    "revision": "108390bfd535930ffdf8",
+    "url": "/static/js/2.5f61e1a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f61e1a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b737d26d65bca641e5",
+    "url": "/static/js/main.800c7835.chunk.js"
+  },
+  {
+    "revision": "fcebea275add5b1af65b",
+    "url": "/static/js/runtime-main.b7afbd3a.js"
+  }
+]);
