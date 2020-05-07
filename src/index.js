46f798d3dd54d7570073df55998dd877355defed
tybeewx <tybeewx@users.noreply.github.com>
2020-05-06 20:00:33 -0400

    scaling

--- src/index.js
+++ src/index.js
@@ -5,24 +5,6 @@ import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      media="screen and (max-width: 360px)"
-      href="portrait.css"
-    ></link>
-    <link
-      rel="stylesheet"
-      type="text/css"
-      media="screen and (min-width: 361px) and (max-width: 480px)"
-      href="landscape.css"
-    ></link>
-    <link
-      rel="stylesheet"
-      type="text/css"
-      media="screen and (min-width: 481px)"
-      href="desktop.css"
-    ></link>
   </React.StrictMode>,
   document.getElementById("root")
 );
